98d3e4bf517b507a6d5b1595de37cc9ff298e820
Alex Middeleer <amiddeleer@gmail.com>
2014-03-04 17:36:59 -0500

    initial commit

---
+++ tail.js
@@ -0,0 +1,49 @@
+// -----------------------------------------------------
+// This file sets up the module for tail; It does every-
+// thing except detect file size changes, which is 
+// implemented by the filewatcher module.
+// -----------------------------------------------------
+var fs = require('fs'),
+	events = require('events'),
+	fileWatcher = require('./fileWatcher.js');
+
+module.exports = {
+
+	startTailing: function(fileName, ms) {
+
+		var fileTailer = new events.EventEmitter(),
+			watcher = fileWatcher.makeFileWatcher(fileName, ms);
+
+		fs.stat(fileName, function(error, info) {
+
+			var originalSize = info.size,
+				currSize = originalSize;
+
+			watcher.on('error', function(error){
+				fileTailer.emit('error',error);
+			});
+			
+			watcher.on('sizeChange', function(newSize) {
+	
+				var readSize = newSize - currSize;
+				if (readSize > 0) {
+					var stream = fs.createReadStream(fileName,{
+						encoding:'utf-8',
+						start:currSize,
+						end:newSize
+					});
+					
+					fileTailer.emit('stream', stream);
+				}
+				currSize = newSize;
+			});
+		
+		});
+
+		return fileTailer;
+	}
+};
+
+					//}).on('data',function(chunk) {
+					//	console.log(chunk.toString());
+					//});
